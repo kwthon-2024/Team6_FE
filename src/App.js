1a4bf7e7027750a70ae72787483ce4b42ccddb92
Junhyok <cjh64866@naver.com>
2024-11-16 15:35:02 +0900

    CHORE: 로그인 페이지 routing endpoint 설정 -> root

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Layout from './layouts/Layout';
-import BottomMenu from './layouts/BottomMenuLayout';
-import Clubs from './pages/clubs/Clubs';
-import ClubsCalendar from './pages/clubs/ClubsCalendar';
-import ClubsDetail from './pages/clubs/ClubsDetail';
-import ClubEvDetail from './pages/ClubEvDetail';
-import ClubEvRegister from './pages/ClubEvRegister';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Graduation from './pages/Graduation';
-import Roadmap from './pages/Roadmap';
-import GraduationKlas from './pages/GraduationKlas';
+import Layout from "./layouts/Layout";
+import BottomMenu from "./layouts/BottomMenuLayout";
+import Clubs from "./pages/clubs/Clubs";
+import ClubsCalendar from "./pages/clubs/ClubsCalendar";
+import ClubsDetail from "./pages/clubs/ClubsDetail";
+import ClubEvDetail from "./pages/ClubEvDetail";
+import ClubEvRegister from "./pages/ClubEvRegister";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Graduation from "./pages/Graduation";
+import Roadmap from "./pages/Roadmap";
+import GraduationKlas from "./pages/GraduationKlas";
 
-import './App.css';
-import './styles/text.css';
-import './styles/utilities.css';
+import "./App.css";
+import "./styles/text.css";
+import "./styles/utilities.css";
 
-import '@fontsource/pretendard';
+import "@fontsource/pretendard";
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/login" element={<Login />} />
+            <Route index element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/pages" element={<BottomMenu />}>
               <Route path="roadmap" element={<Roadmap />} />
