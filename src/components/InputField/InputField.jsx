c8673336184376a31fb709a9aa708457c60b5955
Junhyok <cjh64866@naver.com>
2024-11-16 10:34:43 +0900

    DESIGN: text.css의 p-16 class 제거

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -3,7 +3,7 @@ import "./InputField.css";
 const InputField = ({ title, type = "text", value, onChange }) => {
   return (
     <div className="input-field">
-      {title ? <p className="p-16 c-black">{title}</p> : null}
+      {title ? <p className="p-16-600 c-black">{title}</p> : null}
       <input type={type} value={value} onChange={onChange} />
     </div>
   );
